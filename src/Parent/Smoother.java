012b8e5acec8f3c7f24e09a60dce8f4bbaebb01f
Dustin <dustinmcdonnell@hotmail.com>
2024-03-26 20:46:10 -0400

    Everything Done

--- src/Parent/Smoother.java
+++ src/Parent/Smoother.java
@@ -8,27 +8,47 @@ public class Smoother {
     private ManipulateData md = new ManipulateData();
 
     public void smoothData() {
+        CreateFile cf = new CreateFile();
         File file = md.filePicker();
         ArrayList<String> preFormat = md.readData(file);
+        ArrayList<String> fullArray = new ArrayList<>();
         ArrayList<String> yValue = new ArrayList<>();
+        ArrayList<String> smoothedValue = new ArrayList<>();
         int windowValue = windowValue();
         int yValueCounter = 0;
-        int smoothValue = 0;
 
-        //Pull Y Values into a seperate Array
+        //Pull Y Values into a separate Array
         for (int i = 0; i < preFormat.size(); i++) {
             String[] temp = preFormat.get(i).split(",");
             yValue.add(temp[1]);
         }
 
-        for (int i = 0; i < yValue.size(); i++) {
-            for (int k = -windowValue; k < windowValue; k++) {
-                if (yValue.get(i + k) != null) {
-                    smoothValue = smoothValue + Integer.parseInt(yValue.get(k));
+        for(int i = 0; i < yValue.size(); i++){
+            double counter = 0;
+            double yValueData = 0;
+            for(int k = -windowValue; k <= windowValue; k++){
+                if(i + k >= 0 && i + k < yValue.size()){
+                    yValueData = yValueData + Double.parseDouble(yValue.get(i + k));
+                    counter++;
                 }
             }
-
+            smoothedValue.add(String.valueOf(yValueData/counter));
         }
+
+        for (int i = 0; i < preFormat.size(); i++){
+            String[] temp = preFormat.get(i).split(",");
+            temp[1] = smoothedValue.get(i);
+            fullArray.add(temp[0]);
+            fullArray.add(temp[1]);
+        }
+
+
+
+        System.out.println("Enter the name of the Smooth File you want to create");
+        md.formatCSV(fullArray);
+        File smoothFile = cf.createFile();
+        md.writeData(smoothFile,fullArray);
+
     }
 
     public Integer windowValue(){
